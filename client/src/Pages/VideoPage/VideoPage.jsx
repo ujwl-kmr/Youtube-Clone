dbe8f3cf7aed07d150c06a237b7297344945e546
Ujjawal Kumar <138352226+ujwl-kmr@users.noreply.github.com>
2023-12-02 20:21:29 +0530

    Update VideoPage.jsx

--- client/src/Pages/VideoPage/VideoPage.jsx
+++ client/src/Pages/VideoPage/VideoPage.jsx
@@ -44,7 +44,7 @@ function VideoPage() {
         <div className="container2_videoPage">
           <div className="video_display_screen_videoPage">
             <ReactPlayer
-              url={`youtube-clone-bice-ten.vercel.app/${vv?.filePath}`}
+              url={`https://youtube-clone-bice-ten.vercel.app/${vv?.filePath}`}
               // src={`https://youtubeclone5031.herokuapp.com/${vv?.filePath}`}
               className={"video_ShowVideo_videoPage"}
               controls
