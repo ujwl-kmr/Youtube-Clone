01565ebaa1b13037d74732bba1b133ae4504eb41
Ujjawal Kumar <138352226+ujwl-kmr@users.noreply.github.com>
2023-12-03 12:58:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ import path from "path";
 dontenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://youtubeclone1311.netlify.app/"],
+  }
+));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/uploads", express.static(path.join("uploads")));
